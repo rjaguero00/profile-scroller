dca80119b138a5d5b23b25c963d5e722752bd4c6
Roberto aguero <rjaguero00@gmail.com>
2018-10-08 16:27:53 -0500

    mini project done

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,52 @@ const data = [
         image: 'https://randomuser.me/api/portraits/men/83.jpg'
     }
 ];
+
+const profiles = profileInterator(data);
+
+//Call first profile
+nextProfile();
+
+//next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+//Next Profile Display
+function nextProfile() {
+    const currentProfile = profiles.next().value;
+
+    if (currentProfile !== undefined) {
+        document.getElementById('profileDisplay').innerHTML = `
+    <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+    </ul>
+    <ul class="list-group">
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+    </ul>
+    <ul class="list-group">
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+    </ul>
+    <ul class="list-group">
+        <li class="list-group-item">Preference: ${currentProfile.gender} Looking For ${currentProfile.lookingfor}</li>
+    </ul>
+    `;
+
+        document.getElementById('imageDisplay').innerHTML = `<img
+    src="${currentProfile.image}">`;
+    } else {
+        //No More profiles 
+        window.location.reload();
+    }
+}
+
+//Profile Iterator
+function profileInterator(profiles) {
+    let nextIndex = 0;
+
+    return {
+        next: function () {
+            return nextIndex < profiles.length ?
+                { value: profiles[nextIndex++], done: false } :
+                { done: true }
+        }
+    };
+}
